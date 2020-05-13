ed90a4d3efb19643c7ef4e78de3a5e1cac060aae
Sandro Dolidze <me@sdolidze.ge>
2020-05-13 14:28:02 +0400

    rewrite level01-exclude

--- src/lib/level01-exclude.ts
+++ src/lib/level01-exclude.ts
@@ -1,12 +1,42 @@
 // # Level 01: Exclude
 
-namespace Level01A {
-  type Direction = "up" | "down" | "left" | "right";
+// ## Manual
 
-  type Horizontal = Exclude<Direction, "up" | "down">;
+namespace Level01A {
+  type State =
+    | "idle"
+    | "pending"
+    | "sending"
+    | "receiving"
+    | "processing"
+    | "success"
+    | "failure";
+
+  type BusyState =
+    | "pending"
+    | "sending"
+    | "receiving"
+    | "processing"
+    | "success"
+    | "failure";
 }
 
+// ## Generated
+
 namespace Level01B {
+  type State =
+    | "idle"
+    | "pending"
+    | "sending"
+    | "receiving"
+    | "processing"
+    | "success"
+    | "failure";
+
+  type BusyState = Exclude<State, "idle">;
+}
+
+namespace Level01C {
   type StringOrNumberOrBoolean = string | number | boolean;
 
   type StringOrNumber = Exclude<StringOrNumberOrBoolean, boolean>;
